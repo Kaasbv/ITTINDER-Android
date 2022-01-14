93e3658d448837b769d9f2d7585027b66305acef
Stephan <steef96.sb@gmail.com>
2022-01-14 21:35:41 +0100

    Edit profile page connected to API

--- app/src/main/java/com/ittinder/ittinder/fragment/ProfileFragment.kt
+++ app/src/main/java/com/ittinder/ittinder/fragment/ProfileFragment.kt
@@ -1,21 +1,73 @@
 package com.ittinder.ittinder.fragment
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.ittinder.ittinder.data.RandomUserStream
+import com.ittinder.ittinder.data.User
 import com.ittinder.ittinder.databinding.FragmentProfileBinding
+import com.ittinder.ittinder.viewmodel.UserViewModel
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private val UserData = mutableListOf<User>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val userModel : UserViewModel by viewModels()
+        userModel.getUser()
+
+
+        fun setDataUser(user: User){
+            this.UserData.clear()
+            this.UserData.add(user)
+        }
+
+        fun bindData(user: User){
+            binding.EditTextFirstName.setText(user.firstName)
+            binding.EditTextMiddleName.setText(user.middleName)
+            binding.EditTextSurname.setText(user.surname)
+            binding.EditTextDoB.setText(user.dateOfBirth)
+            binding.EditDescription.setText(user.description)
+            binding.EditTextEmail.setText(user.email)
+        }
+
+        fun bindDataAfterSend(user: User) {
+            user.firstName = binding.EditTextFirstName.text.toString()
+            user.middleName = binding.EditTextMiddleName.text.toString()
+            user.surname = binding.EditTextSurname.text.toString()
+            user.dateOfBirth = binding.EditTextDoB.text.toString()
+            user.description = binding.EditDescription.text.toString()
+            user.email = binding.EditTextEmail.text.toString()
+            user.password = binding.EditTextPassword.text.toString()
+            user.middleName = binding.EditTextMiddleName.text.toString()
+            setDataUser(user)
+
+
+        }
+
+
+            userModel.status.observe(this) {
+                val returnValue = userModel.status.value
+                if (returnValue != null) {
+                    setDataUser(returnValue)
+                    bindData(returnValue)
+                    binding.login.setOnClickListener {
+                        bindDataAfterSend(returnValue)
+                        userModel.updateUser(UserData[0])
+
+                }
+            }
+        }
         return binding.root
     }
 
--- app/src/main/java/com/ittinder/ittinder/service/UserApiService.kt
+++ app/src/main/java/com/ittinder/ittinder/service/UserApiService.kt
@@ -2,14 +2,12 @@ package com.ittinder.ittinder.service
 
 import com.ittinder.ittinder.data.RandomUserStream
 import com.ittinder.ittinder.data.User
+import com.squareup.moshi.Json
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.POST
-import retrofit2.http.PUT
+import retrofit2.http.*
 
 private const val BASE_URL = "http://10.0.2.2:8080"
 
@@ -29,12 +27,12 @@ interface UserApiService {
     suspend fun createUser(): User
 
     @GET("/user")
-    //TODO test this function!!!
     suspend fun getUser(@Header("Cookie") session_id: String): User
 
     @PUT("/user/update")
     //TODO test this function!!!
-    suspend fun updateUser(): User
+    suspend fun updateUser(@Header("Cookie") session_id: String, @Body user: User): String
+
 }
 
 object UserApi {
