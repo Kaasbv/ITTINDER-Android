c27cf21389402f01465e35853f3444aa897911a4
Stephan <steef96.sb@gmail.com>
2022-01-15 18:45:17 +0100

    Fixed user entity

--- app/src/main/java/com/ittinder/ittinder/data/User.kt
+++ app/src/main/java/com/ittinder/ittinder/data/User.kt
@@ -8,23 +8,13 @@ import java.time.LocalDate
 data class User(
     @PrimaryKey(autoGenerate = false)
     val id: Long,
-    val firstName: String,
-    val surname: String,
-    val dateOfBirth: String,
-    val email: String,
-    val gender: String?,
-    val description: String?,
-    val interestedInGender: String?
-)
-    var id: Int,
     var firstName: String,
     var middleName: String,
     var surname: String,
     var dateOfBirth: String,
     var email: String,
-    var gender: String,
-    var description: String,
-    var interestedInGender: String,
+    var gender: String?,
+    var description: String?,
+    var interestedInGender: String?,
     var password: String
-) {
-}
+)
